55a2b9dd8692d6d4cefc014c3f1555f13ef0b8d4
Arya Veda <vedakhansa@gmail.com>
2026-01-02 01:08:15 +0700

    fix bug image uplaod on dashboard filament2

--- api/index.php
+++ api/index.php
@@ -1,2 +1,30 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// === TAMBAHKAN BAGIAN INI ===
+// Vercel adalah lingkungan Read-Only.
+// Kita harus memindahkan storage path ke /tmp (satu-satunya folder yang bisa ditulis).
+
+$storagePath = '/tmp/storage';
+
+// Cek apakah folder storage di /tmp sudah ada? Kalau belum, buat strukturnya.
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+    mkdir($storagePath . '/framework/views', 0777, true);
+    mkdir($storagePath . '/framework/cache', 0777, true); // Ini untuk mengatasi error facade-...
+    mkdir($storagePath . '/framework/sessions', 0777, true);
+    mkdir($storagePath . '/app', 0777, true);
+    mkdir($storagePath . '/logs', 0777, true);
+}
+
+// Perintahkan Laravel untuk menggunakan path baru ini sebagai 'storage'
+$app->useStoragePath($storagePath);
+// ============================
+
+$request = Illuminate\Http\Request::capture();
+$response = $app->handle($request);
+$response->send();
+$app->terminate();
